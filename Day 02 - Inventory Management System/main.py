20c0fab9e1bb967ab515d52d30fb4e580f187eb0
Kevin van der Werff <Kevinsebastianate@hotmail.com>
2018-12-02 10:44:33 +0100

    Implementation part 1

--- Day 02 - Inventory Management System/main.py
+++ Day 02 - Inventory Management System/main.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import fileinput
 
 
@@ -8,8 +9,26 @@ def transform_data(filename):
     return values
 
 
+def count_valid_boxes(acc, curr):
+    twos = 0
+    threes = 0
+
+    for char in curr:
+        count = curr.count(char)
+        if (twos == 0 and count == 2):
+            twos = 1
+        if (threes == 0 and count == 3):
+            threes = 1
+        if (twos == 1 and threes == 1):
+            break
+
+    return (acc[0] + twos, acc[1] + threes)
+
+
 def part_1():
     values = transform_data('puzzle-input.txt')
+    count = reduce(count_valid_boxes, values, (0, 0))
+    return count[0] * count[1]
 
 
-print(part_1())
+print(part_1()) # 8715
