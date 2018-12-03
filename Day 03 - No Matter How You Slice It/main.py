214097b58188136b016a6ee2ced186df2d347e4b
Kevin van der Werff <Kevinsebastianate@hotmail.com>
2018-12-03 11:55:55 +0100

    Add transformation of data

--- Day 03 - No Matter How You Slice It/main.py
+++ Day 03 - No Matter How You Slice It/main.py
@@ -3,6 +3,22 @@ import unittest
 import re
 
 
+def file_to_list(filename):
+    values = []
+    for line in fileinput.input(filename):
+        values.append(line.strip())
+    return values
+
+
+def get_digits(list):
+    return [int(entry) for entry in list if entry.isdigit()]
+
+
+def transform_data(list):
+    split = [re.split(' |x|,|:', entry) for entry in list]
+    return [get_digits(entry) for entry in split]
+
+
 class Test_Part_1(unittest.TestCase):
     def test_transform_data(self):
         values = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
@@ -11,5 +27,4 @@ class Test_Part_1(unittest.TestCase):
             [3, 1, 4, 4],
             [5, 5, 2, 2],
         ]
-        result = transform_data(values)
-        self.assertEqual(result, expected)
+        self.assertEqual(transform_data(values), expected)
