b7e60aae2d3024567a34a2aefff31acb16a2dd9e
Kevin van der Werff <Kevinsebastianate@hotmail.com>
2018-12-03 12:02:29 +0100

    Add simple test part 01

--- Day 03 - No Matter How You Slice It/main.py
+++ Day 03 - No Matter How You Slice It/main.py
@@ -19,6 +19,10 @@ def transform_data(list):
     return [get_digits(entry) for entry in split]
 
 
+# Part 1 function entry
+def amount_overclaimed_squares(list):
+    return 0
+
 class Test_Part_1(unittest.TestCase):
     def test_transform_data(self):
         values = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
@@ -28,3 +32,8 @@ class Test_Part_1(unittest.TestCase):
             [5, 5, 2, 2],
         ]
         self.assertEqual(transform_data(values), expected)
+
+    def test_simple(self):
+        values = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
+        data = transform_data(values)
+        self.assertEqual(amount_overclaimed_squares(data), 4)
